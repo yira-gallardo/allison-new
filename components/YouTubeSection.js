7d9a0da83ea0c7b893d404b78cba43b213da36e5
Vannelo <work@vannelo.com>
2025-11-11 02:00:00 -0600

    Add change

--- components/YouTubeSection.js
+++ components/YouTubeSection.js
@@ -2,8 +2,8 @@ import DynamicVideo from "./DynamicVideo";
 
 export default function YouTubeSection() {
   // YouTube API configuration from environment variables
-  const channelHandle = process.env.NEXT_PUBLIC_YOUTUBE_CHANNEL_HANDLE || "@allison";
-  const apiKey = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY || "";
+  const channelHandle = process.env.YOUTUBE_CHANNEL_HANDLE || "@allison";
+  const apiKey = process.env.YOUTUBE_API_KEY || "";
 
   return <DynamicVideo channelHandle={channelHandle} apiKey={apiKey} />;
 }
